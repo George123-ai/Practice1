56b21d56ee1383b2977c5771fa606f3c89a23699
Georgy Pogosyan <34yarikbro2@gmail.com>
2023-02-21 12:27:51 +0400

    Add project files.

---
+++ Practice1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System.Data;
+
+namespace Practice1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int num1 = 5;
+            //ChangeValue(num1);
+            //Console.WriteLine(num1);
+
+            int num2 = ChangeValue(ref num1);
+            Console.WriteLine(num2);
+
+            ChangeValue1(out num1);
+            Console.WriteLine(num1);
+
+            ChangeValue1(out int num3);
+            Console.WriteLine(num3);
+
+            int num4 = 5;
+            ChangeValue2(num4);
+            
+        }
+
+        static int ChangeValue(ref int value)
+        {
+            value++;
+            return value;
+        }
+
+        static void ChangeValue1(out int value)
+        {
+            value = 100;
+        }
+
+        static void ChangeValue2(in int value)
+        {
+            Console.WriteLine(value);
+        }
+
+        
+    }
+}
