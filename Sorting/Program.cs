881c826b16ab5740e5bdc15b545a0a71aec08a5a
Georgy Pogosyan <34yarikbro2@gmail.com>
2023-03-26 18:31:52 +0400

    added code

---
+++ Sorting/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Diagnostics;
+
+namespace Sorting
+{
+    internal class Program
+    {
+        static int BinarySearch(int[] array , int target)
+        {
+            int step = 1;
+            int low = 0;
+            int high = array.Length - 1;
+            while (low <= high)
+            {
+                int middle = low + (high - low) / 2;
+                int value = array[middle];
+                
+                Console.WriteLine($"middle: {value} , step: {step}");
+                step++;
+                if (value < target)
+                    low = middle + 1;
+                else if (value > target)
+                    high = middle - 1;
+                else
+                    return middle;
+            }
+
+            return -1;
+        }
+
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello, World!");
+            
+            int[] array = new int[100];
+
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+                //Console.WriteLine(array[i]);
+            }
+            int target = 99;
+            int index = BinarySearch(array, target);
+
+            if(index == -1)
+                Console.WriteLine($"{target} not found!");
+            else
+                Console.WriteLine($"Target found at index: {index}");
+        }
+    }
+}
