db0df320d2feb499a8cd78878d54da7bf4d6965b
Georgy Pogosyan <34yarikbro2@gmail.com>
2023-03-26 14:50:25 +0400

    added code

---
+++ JsonTxtReadWrite/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace JsonTxtReadWrite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string txtFilePath = @"C:\test\test1.txt";
+
+
+            string[] lines = File.ReadAllLines(txtFilePath);
+            for (int i = 0; i < lines.Length; i++)
+            {
+                Console.WriteLine(lines[i]);
+            }
+
+            //List<string> lines = new List<string>();
+
+        }
+    }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace Lesson9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[101];
+            int x = 24;
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                numbers[i] = i;
+                Console.WriteLine(numbers[i]);
+            }
+
+            //for (int i = 0; i < numbers.Length; i++)
+            //{
+
+            //}
+            int half = numbers[(numbers.Length - 1) / 2];
+            //Console.WriteLine("/////////////" + half / 2 / 2);
+            if (half > x) // 50 > 24 --> true
+            {
+                if (half/2 > x) // 25 > 24 --> true
+                {
+                    if (half/2/2 > x) // 12 > 24 --> false
+                    {
+
+                    }
+                    else if(half/2/2 == x)
+                    {
+                        Console.WriteLine($" index: {x}, value: {numbers[x]}");
+                    }
+                    else
+                    {
+                        if ((half/2/2)*2 > x)
+                        {
+
+                        }
+                        else if((half/2/2)*2 == x)
+                        {
+                            Console.WriteLine($" index: {x}, value: {numbers[x]}");
+                        }
+                    }
+                }
+                else if (half == x)
+                {
+                    Console.WriteLine($" index: {x}, value: {numbers[x]}");
+                }
+            }
+            else if(half== x)
+            {
+                Console.WriteLine($" index: {x}, value: {numbers[x]}");
+            }
+
+            
+        }
+    }
+}
