56dac37dde30fb33e2108e401113a54b0abf51c3
Georgy Pogosyan <34yarikbro2@gmail.com>
2023-03-26 16:40:16 +0400

    added code

---
+++ JsonTxtReadWrite/Person.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace JsonTxtReadWrite
+{
+    internal class Person
+    {
+        public Person(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+
+        public string Name { get; set; }
+        public string Surname { get; }
+
+        public override string ToString()
+        {
+            return $"Name:{Name},Surname:{Surname}";
+        }
+
+    }
+}
--- JsonTxtReadWrite/Program.cs
+++ JsonTxtReadWrite/Program.cs
@@ -5,15 +5,56 @@
         static void Main(string[] args)
         {
             string txtFilePath = @"C:\test\test1.txt";
+            List<Person> people = new List<Person>();
+            List<string> lines = new List<string>();
+            
 
+            lines = File.ReadAllLines(txtFilePath).ToList();
 
-            string[] lines = File.ReadAllLines(txtFilePath);
-            for (int i = 0; i < lines.Length; i++)
+            for (int i = 0; i < lines.Count; i++)
             {
-                Console.WriteLine(lines[i]);
+                string[] items = lines[i].Split(',');
+                Person person = new Person(items[0], items[1]);
+                people.Add(person);
+
             }
-            //
-            //List<string> lines = new List<string>();
+
+            List<string> list1 = new List<string>(); 
+
+            for (int i = 0; i < people.Count; i++)
+            {
+                Console.WriteLine(people[i]);
+                list1.Add(people[i].ToString());
+
+            }
+
+            //FileStream fs = File.Create("test2");
+
+            string jsonFilePath = @"C:\test\testJson1.json";
+            string[] jsonLines = File.ReadAllLines(jsonFilePath);
+            for (int i = 0; i < jsonLines.Length; i++)
+            {
+                Console.WriteLine(jsonLines[i]);
+            }
+
+            //string[] lines = File.ReadAllLines(txtFilePath);
+            ////for (int i = 0; i < lines.Length; i++)
+            ////{
+            ////    Console.WriteLine(lines[i]);
+            ////}
+            
+            //List<string> lines1 = new List<string>();
+            //lines1 = File.ReadAllLines(txtFilePath).ToList();
+
+            //for (int i = 0; i < lines1.Count; i++)
+            //{
+            //    Console.WriteLine(lines1[i]);
+            //}
+
+            //lines1.Add("Name:Bruce,Surname:Wayne");
+            //Person person = new Person();
+            
+            //File.WriteAllLines(txtFilePath, lines1);
 
         }
     }
