e935506baab954a2ca5dca7d4bf95dd974f1c128
Georgy Pogosyan <34yarikbro2@gmail.com>
2023-03-27 15:32:55 +0400

    added code

--- Sorting/Program.cs
+++ Sorting/Program.cs
@@ -27,6 +27,33 @@ namespace Sorting
             return -1;
         }
 
+        static int[] BubbleSortArray(int[] arr) // BubbleSort
+        {
+            //int[] result = new int[arr.Length];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                for (int j = 0;  j < arr.Length-1;  j++)
+                {
+                    if (arr[j] > arr[j+1])
+                    {
+                        int x = arr[j];
+                        arr[j] = arr[j + 1];
+                        arr[j + 1] = x;
+                    }
+                }
+            }
+
+            return arr;
+        }
+
+        static void PrintArr<T>(T[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                Console.Write($" {arr[i]} ");
+            }
+        }
+
         static void Main(string[] args)
         {
             //Console.WriteLine("Hello, World!");
@@ -38,13 +65,26 @@ namespace Sorting
                 array[i] = i;
                 //Console.WriteLine(array[i]);
             }
-            int target = 99;
-            int index = BinarySearch(array, target);
+            //int target = 99;
+            //int index = BinarySearch(array, target);
 
-            if(index == -1)
-                Console.WriteLine($"{target} not found!");
-            else
-                Console.WriteLine($"Target found at index: {index}");
+            //if(index == -1)
+            //    Console.WriteLine($"{target} not found!");
+            //else
+            //    Console.WriteLine($"Target found at index: {index}");
+
+            int[] arr = new int[11];
+            Random rnd = new Random();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = rnd.Next(0, 11);
+            }
+            
+
+            PrintArr(arr);
+            Console.WriteLine();
+            
+            PrintArr(BubbleSortArray(arr));
         }
     }
 }
